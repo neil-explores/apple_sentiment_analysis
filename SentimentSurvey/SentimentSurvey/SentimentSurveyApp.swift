29c94c72cb915ffceaa84be05e6a402b519caed7
Neil Thorne <neilsthorne@gmail.com>
2026-03-07 22:22:30 -0500

    init commit

---
+++ SentimentSurvey/SentimentSurvey/SentimentSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentSurveyApp.swift
+//  SentimentSurvey
+//
+//  Created by Neil Thorne on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SentimentSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
